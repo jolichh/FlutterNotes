98b3a8ca9db079ee19ccc905a67e203642730f2b
Xavi <xavi.quesada@iticbcn.cat>
2024-02-20 18:26:25 +0100

    avance version 2

--- app_notes/lib/pages/home.dart
+++ app_notes/lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:app_notes/pages/listaDeNotas.dart';
 import 'package:app_notes/pages/note.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
@@ -28,9 +29,47 @@ class _homePageState extends State<Home> {
                 AgregarModificarNota(nota: nota, esNueva: esNueva)));
   }
 
+  void eliminarNota(Nota nota) {
+    Provider.of<listaDeNotas>(context, listen: false).deleteNota(nota);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Consumer<listaDeNotas>(
+      builder: (context, note, child) => Scaffold(
+        backgroundColor: CupertinoColors.systemBackground,
+        floatingActionButton: FloatingActionButton(
+          onPressed: createNotas,
+          child: Icon(Icons.add),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //header
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'my Notes',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            //lista de notas
+            CupertinoListSection.insetGrouped(
+                children: List.generate(
+                    note.getNotas().length,
+                    (index) => CupertinoListTile(
+                          title: Text(note.getNotas()[index].contenido),
+                          onTap: () =>
+                              navegarNota(note.getNotas()[index], false),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () =>
+                                eliminarNota(note.getNotas()[index]),
+                          ),
+                        ))),
+          ],
+        ),
+      ),
+    );
   }
 }
--- app_notes/lib/pages/note.dart
+++ app_notes/lib/pages/note.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:app_notes/pages/listaDeNotas.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
